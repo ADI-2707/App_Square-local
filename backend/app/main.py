3f02c5d81c7ed606818eb6d4a877f967077e9952
ADI-2707 <aditya21singh2707@gmail.com>
2026-02-16 12:13:41 +0530

    added in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.database import Base, engine, SessionLocal
 from app.models import user, log
 from app.services.auth_service import initialize_system_users
-from app.routes import auth_routes, admin_routes
+from app.routes import auth_routes, admin_routes, template_routes
 from app.services.log_service import cleanup_old_logs
 from app.utils.error_middleware import ExceptionLoggingMiddleware
 
@@ -23,3 +23,4 @@ def startup_event():
 
 app.include_router(auth_routes.router)
 app.include_router(admin_routes.router)
+app.include_router(template_routes.router)
