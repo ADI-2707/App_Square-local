5ed96ae9cbe4fcfdcfcb734920f1c821a9938b89
ADI-2707 <aditya21singh2707@gmail.com>
2026-02-17 13:16:39 +0530

    integrated EntityProvider into app root

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext/AuthContext";
+import { EntityProvider } from "./context/EntityContext/EntityContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <EntityProvider>
+          <App />
+        </EntityProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
