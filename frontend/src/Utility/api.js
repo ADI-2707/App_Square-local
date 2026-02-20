35c97df51f634d4602194237404d9df79528642e
ADI-2707 <aditya21singh2707@gmail.com>
2026-02-20 12:42:09 +0530

    fixed redirection

--- frontend/src/Utility/api.js
+++ frontend/src/Utility/api.js
@@ -19,7 +19,7 @@ api.interceptors.response.use(
   (error) => {
     if (error.response?.status === 401) {
       localStorage.removeItem("access_token");
-      window.location.href = "/login";
+      window.location.href = "/";
     }
     return Promise.reject(error);
   }
