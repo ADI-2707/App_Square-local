144e2d9c5cf8e0adfe278a314ac7a09aca0f70b1
ADI-2707 <aditya21singh2707@gmail.com>
2026-02-24 15:36:18 +0530

    restored initialize_system_users after refactor

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -3,10 +3,11 @@ from datetime import datetime
 from fastapi import HTTPException, status
 
 from app.schemas.user_schema import LoginRequest
-from app.utils.security import verify_password
+from app.utils.security import verify_password, hash_password
 from app.utils.jwt_handler import create_access_token
 from app.services.log_service import add_log
 from app.queries import user_queries
+from app.config import ROOT_ADMIN_PASSWORD, GUEST_PASSWORD
 
 MAX_ATTEMPTS = 5
 BLOCK_DURATION_MINUTES = 5
@@ -121,3 +122,37 @@ def login_user(request: LoginRequest, db: Session):
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Internal server error during login"
         )
+
+
+def initialize_system_users(db: Session):
+
+    if not ROOT_ADMIN_PASSWORD or not GUEST_PASSWORD:
+        raise ValueError("System user passwords must be set in environment variables.")
+
+    try:
+        admin = user_queries.get_by_username(db, "admin")
+        guest = user_queries.get_by_username(db, "guest")
+
+        if not admin:
+            user_queries.create_user(
+                db=db,
+                username="admin",
+                hashed_password=hash_password(ROOT_ADMIN_PASSWORD),
+                role="admin"
+            )
+        else:
+            user_queries.update_role(db, admin, "admin")
+
+        if not guest:
+            user_queries.create_user(
+                db=db,
+                username="guest",
+                hashed_password=hash_password(GUEST_PASSWORD),
+                role="guest"
+            )
+
+        db.commit()
+
+    except Exception:
+        db.rollback()
+        raise
