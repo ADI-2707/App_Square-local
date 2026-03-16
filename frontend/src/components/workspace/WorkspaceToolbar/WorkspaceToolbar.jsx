4c815410da4b23731d98c9320a60a0f7e8030bec
ADI-2707 <aditya21singh2707@gmail.com>
2026-03-16 16:29:33 +0530

    added HMI-style control indicators to workspace toolbar and refined button alignment

--- frontend/src/components/workspace/WorkspaceToolbar/WorkspaceToolbar.jsx
+++ frontend/src/components/workspace/WorkspaceToolbar/WorkspaceToolbar.jsx
@@ -3,31 +3,21 @@ import "./workspaceToolbar.css";
 export default function WorkspaceToolbar({ onUpload, onDownload }) {
   return (
     <div className="workspace-toolbar">
-
-      <div className="toolbar-title">
-        Workspace Controls
+      <div className="toolbar-left">
+        <span className="toolbar-title">Workspace Controls:</span>
       </div>
 
       <div className="toolbar-actions">
-
-        <button
-          className="hmi-btn"
-          onClick={onUpload}
-        >
+        <button className="hmi-btn" onClick={onUpload}>
           <img src="/icons/upload.svg" className="btn-icon" />
           Upload
         </button>
 
-        <button
-          className="hmi-btn"
-          onClick={onDownload}
-        >
+        <button className="hmi-btn" onClick={onDownload}>
           <img src="/icons/download.svg" className="btn-icon" />
           Download
         </button>
-
       </div>
-
     </div>
   );
 }
